63de3607b44c498276de94b099d879c047b5ac31
JavaMan7 <jordan200414@gmail.com>
2016-12-09 17:34:37 -0600

    Light in sub dimension

--- src/main/java/com/javaman/subterranean/dimension/WorldProviderSub.java
+++ src/main/java/com/javaman/subterranean/dimension/WorldProviderSub.java
@@ -2,13 +2,16 @@ package com.javaman.subterranean.dimension;
 
 import com.javaman.subterranean.biomes.BiomeRegistry;
 
+import net.minecraft.world.WorldProvider;
 import net.minecraft.world.WorldProviderSurface;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.WorldChunkManagerHell;
 import net.minecraft.world.chunk.IChunkProvider;
 
-public class WorldProviderSub extends WorldProviderSurface{
-
+public class WorldProviderSub extends WorldProvider //WorldProviderSurface
+{
+	//** Light to brightness conversion table */
+    
 	@Override
 	public String getDimensionName() {
 		
@@ -47,7 +50,29 @@ public class WorldProviderSub extends WorldProviderSurface{
             this.lightBrightnessTable[var2] = (1.0F - var3) / (var3 * 3.0F + 1.0F) * (1.0F - var1) + var1;
         }
     }
-	
+	/* @Override
+	 protected void generateLightBrightnessTable()
+	    {
+	        float f = 0.0F;
+
+	        for (int i = 0; i <= 15; ++i)
+	        {
+	            float f1 = 1.0F - (float)i / 15.0F;
+	            this.lightBrightnessTable[i] = (1.0F - f1) / (f1 * 3.0F + 1.0F) * (1.0F - f) + f;
+	        }
+	    }
+
+	@Override
+	public String getInternalNameSuffix() {
+		return "";
+		
+	}*/
+
+	@Override
+	public String getInternalNameSuffix() {
+		// TODO Auto-generated method stub
+		return " ";
+	}
 	
 	
 
